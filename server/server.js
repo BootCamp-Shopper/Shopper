07eb69ed16accc4efb7c2b270e300ef55d7dcd0f
unknown <matthewpiedra1228@gmail.com>
2020-07-28 17:31:08 -0400

    Added error catching function and init to sync database as well as server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const db = require("./db");
+const { db } = require("./db");
 const morgan = require("morgan");
 const port = 4000;
 
@@ -8,6 +8,24 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-   console.log(`Server started on Port ${port}`);
+// Error catching endware
+app.use((err, req, res, next) => {
+   console.error(err, typeof next)
+   console.error(err.stack)
+   res.status(err.status || 500).send(err.message || 'Internal server error.')
 });
+
+const init = async () => {
+ try {
+   await db.sync();
+
+   app.listen(port, () => {
+      console.log(`App listening on PORT ${port}`)
+   });
+  }
+  catch(err) {
+     console.log(err);
+  } 
+}
+
+init();

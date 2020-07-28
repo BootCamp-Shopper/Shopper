db4d21c7ad007e8699ebafbaa5781b8aca8618ee
unknown <aishaben.18@gmail.com>
2020-07-28 17:39:06 -0400

    commit

--- server/api/items.js
+++ server/api/items.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const { Items, User } = require("../db");
+const { Item, User } = require("../db");
 
 //Get All Items
 router.get("/", async (req, res, next) => {
   try {
-    const items = await Items.findAll({
-      include: [User],
+    const items = await Item.findAll({
+      include: [{ model: User }],
     });
     res.send("this is items routes");
   } catch (error) {
@@ -17,7 +17,7 @@ router.get("/", async (req, res, next) => {
 router.get("/:itemsId", async (req, res, next) => {
   try {
     const items = await Item.findById(req.params.albumId, {
-      include: [Item, User],
+      include: [{ model: Item }, { model: User }],
     });
     res.json(items);
   } catch (error) {
--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const itemsRoute = require("./api/items");
 const port = 4000;
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api", itemsRoute);
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
 app.listen(port, () => {
-   console.log(`Server started on Port ${port}`);
+  console.log(`Server started on Port ${port}`);
 });

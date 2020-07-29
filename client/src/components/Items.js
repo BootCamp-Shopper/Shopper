9472cfa6c8e39311b1468926e41fe6aad3c7ccb8
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-29 16:35:26 -0400

    ADDED /client/components/items for all items

--- client/src/components/Items.js
+++ client/src/components/Items.js
@@ -1,10 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class Items extends Component {
-    render () {
+    constructor() {
+        super();
+        this.state = {
+            items: [],
+            loading: true,
+        };
+    };
+
+    async componentDidMout() {
+        try {
+            const response = await fetch('/api/items', { method: 'GET' });
+            const itemsData = await response.json();
+            this.setState({
+                items: itemsData.results,
+                loading: false,
+            });
+        } catch (err) {
+            console.error('ERROR: ', err);
+        };
+    };
+
+    render() {
+        const { items, loading } = this.state;
+        if (items.length === 0) {
+            return <div>No item found!</div>
+        };
+        if (loading) {
+            return <div>Loading...</div>
+        };
+
         return (
             <div>
+                <div>
+                    {items.map(item => {
+                        return
+                        <div>
+                            <div>{item.name}</div>
+                            <img src={item.imageUrl} alt={item.name}/>
+                            <div>{item.description}</div>
+                            <div>{item.price}</div>
+                        </div>
+                    })}
+                </div>
             </div>
-        )
-    }
-}
+        );
+    };
+};

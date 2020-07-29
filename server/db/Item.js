a3dad118d90d2c2fd95b604aaa8574ce9b0c8328
unknown <aishaben.18@gmail.com>
2020-07-29 11:25:48 -0400

    items_routes

--- server/db/Item.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-const db = require("./db");
-
-const Item = db.define("item", {
-  name: Sequelize.STRING,
-  Url: Sequelize.STRING,
-});
-
-module.exports = Item;

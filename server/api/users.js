60a7e8a5b08c7c71593303da83b6388ba68950da
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-07-31 00:28:21 -0400

    ADDED admin's api/users and api/users/usersId routes

--- server/api/users.js
+++ server/api/users.js
@@ -1,23 +1,28 @@
 const router = require("express").Router();
-const {User} = require('../db/index')
+const { User } = require('../db/index')
 
 //all users route
 router.get('/', async (req, res, next) => {
     try {
         const users = await User.findAll({
             //use attributes to pull non sensitive information from db
-            attributes: ['id', 'firstName', 'lastName', 'imageUrl', 'address', 'email', 'role'] 
-        })
-        
+            attributes: ['id', 'firstName', 'lastName', 'imageUrl', 'address', 'email', 'role']
+        });
         //exports the queried info to front end in json array
         res.send(users);
-        
-        
     } catch (error) {
         next(error)
     }
-    
-})
+});
+
+router.get('/:usersId', async(req, res, next) => {
+    try {
+        const user = await User.findByPk(req.params.usersId);
+        res.send(user);
+    } catch (error) {
+        next(error)
+    }
+});
 
 
 module.exports = router;

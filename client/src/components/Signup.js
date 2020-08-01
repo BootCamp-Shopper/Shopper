209febaf9c35608e9eeebe8bd2d6c5bf8f150180
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-08-01 02:14:57 -0400

    SIGNUP FORM - changed 'name' to 'firstName' and 'lastName'

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,20 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import SignupForm from './SignupForm';
 
-export default class Signup extends Component{
-    constructor(){
+export default class Signup extends Component {
+    constructor() {
         super()
-        this.handleSubmit = this.handleSubmit.bind(this) //binding the context of the class to remember the function's use when passed to props
+        this.handleSubmit = this.handleSubmit.bind(this) 
+        //binding the context of the class to remember the function's use when passed to props
     }
 
-    async handleSubmit(event){
+    async handleSubmit(event) {
         //prevent reloading of page onClick
         event.preventDefault()
 
         //variables containing user's input values
         const email = event.target.email.value;
-        const name = event.target.name.value;
+        const firstName = event.target.firstName.value;
+        const lastName = event.target.lastName.value;
         const password = event.target.password.value;
         const address = event.target.address.value;
         const imageUrl = event.target.imageUrl.value;
@@ -23,19 +25,20 @@ export default class Signup extends Component{
         let res;
 
         try {
-            res = await axios.post('/auth/signup', {email, name, password, address, imageUrl})
-
+            res = await axios.post('/auth/signup', {
+                email, firstName, lastName, password, address, imageUrl
+            })
         } catch (error) {
             console.error(error)
         }
-        
-        
+
+
     }
 
-    render(){
+    render() {
         return (
             <div>
-                <SignupForm handleSubmit={this.handleSubmit}/>
+                <SignupForm handleSubmit={this.handleSubmit} />
             </div>
         )
     }
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,60 +1,69 @@
 import React from 'react';
 
-function SignupForm (props){
-    const {handleSubmit} = props
+function SignupForm(props) {
+    const { handleSubmit } = props
     return (
         <div>
-            <form onSubmit= {handleSubmit}>
-            <div>
-                <label htmlFor='email'>
-                    <small>
-                        Email:
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='email'>
+                        <small>
+                            Email:
                     </small>
-                </label>
-                <input name='email' type='text'/>
-            </div>
-            
-            <div>
-            <label htmlFor='password'>
-                    <small>
-                        Password:
-                    </small>
-                </label>
-                <input name='password' type='password'/>
-            </div>
-            
-            <div>
-            <label htmlFor='name'>
-                    <small>
-                        Name:
-                    </small>
-                </label>
-                <input name='name' type='text'/>
-            </div>
+                    </label>
+                    <input name='email' type='text' />
+                </div>
 
-            <div>
-            <label htmlFor='address'>
-                    <small>
-                        Address:
+                <div>
+                    <label htmlFor='password'>
+                        <small>
+                            Password:
                     </small>
-                </label>
-                <input name='address' type='text'/>
-            </div>
+                    </label>
+                    <input name='password' type='password' />
+                </div>
 
-            <div>
-            <label htmlFor='imageUrl'>
-                    <small>
-                        ImageUrl:
+                <div>
+                    <label htmlFor='name'>
+                        <small>
+                            First Name:
                     </small>
-                </label>
-                <input name='imageUrl' type='text'/>
-            </div>
+                    </label>
+                    <input name='name' type='text' />
+                </div>
 
-            <div>
-                <button type='submit'>
-                    Sign Up
+                <div>
+                <label htmlFor='name'>
+                        <small>
+                            Last Name:
+                    </small>
+                    </label>
+                    <input name='name' type='text' />
+                </div>
+
+                <div>
+                    <label htmlFor='address'>
+                        <small>
+                            Address:
+                    </small>
+                    </label>
+                    <input name='address' type='text' />
+                </div>
+
+                <div>
+                    <label htmlFor='imageUrl'>
+                        <small>
+                            ImageUrl:
+                    </small>
+                    </label>
+                    <input name='imageUrl' type='text' />
+                </div>
+
+                <div>
+                    <button type='submit'>
+                        Sign Up
                 </button>
-            </div>
+                </div>
 
             </form>
         </div>

3e9e0bde1e7d4fe12728e05eb57e542f296a18c7
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-08-03 20:37:10 -0400

    ADDED db/address model

--- server/db/user.js
+++ server/db/user.js
@@ -15,10 +15,6 @@ const User = db.define('user', {
         type: Sequelize.STRING,
         defaultValue: 'default-user.jpg',
     },
-    address: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
     email: {
         type: Sequelize.STRING,
         allowNull: false,

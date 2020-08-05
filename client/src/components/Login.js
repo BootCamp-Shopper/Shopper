0c1947300537a10e729999a8ac84c7acdd66f04a
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-08-05 16:35:35 -0400

    CREATED Login Authentication middleware and APPLIED to /cart, /users pages for redirecting members and guests

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Button, FormGroup, FormControl, FormLabel } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Axios from 'axios';
+import Items from './Items';
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [items, setItems] = useState([]);
 
     function validateForm() {
         return email.length > 0 && password.length > 0;
@@ -15,48 +17,57 @@ export default function Login() {
         e.preventDefault();
 
         try {
-            const {data} = await Axios.post('/auth/login', {
+            const { data } = await Axios.post('/auth/login', {
                 email: email,
                 password: password,
             });
-            console.log(data);
+            if (Array.isArray(data)) {
+                setItems(data);
+            }
+            else if (data === 'Login failed') {
+                window.alert(data)
+            }
         }
-        catch(err) {
+        catch (err) {
             console.error(err);
         }
     }
 
-    return (
-        <div className="Login">
-            <form onSubmit={handleSubmit}>
-                <FormGroup controlId="email" bssize="large">
-                    <FormLabel>E-mail:</FormLabel>
-                    <FormControl
-                        autoFocus
-                        type="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup controlId="password" bssize="large">
-                    <FormLabel>Password:</FormLabel>
-                    <FormControl
-                        type="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                </FormGroup> 
-                <Button block bssize="large" disabled={!validateForm()} type="submit">
-                    Login
-                </Button>
-                <br /><br />
-                <div>Not part of superpower community? Join us!</div>
-                <Link to="/signup">
-                    <Button block bssize="large" type="submit">
-                        Sign Up
+    if (items.length > 0) {
+        return <Redirect to='/superpowers' component={Items} />
+    } else {
+        return (
+            <div className="Login">
+                <form onSubmit={handleSubmit}>
+                    <FormGroup controlId="email" bssize="large">
+                        <FormLabel>E-mail:</FormLabel>
+                        <FormControl
+                            autoFocus
+                            type="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </FormGroup>
+                    <FormGroup controlId="password" bssize="large">
+                        <FormLabel>Password:</FormLabel>
+                        <FormControl
+                            type="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </FormGroup>
+                    <Button block bssize="large" disabled={!validateForm()} type="submit">
+                        Login
                     </Button>
-                </Link>
-            </form>
-        </div>
-    );
+                    <br /><br />
+                    <div>Not part of superpower community? Join us!</div>
+                    <Link to="/signup">
+                        <Button block bssize="large" type="submit">
+                            Sign Up
+                        </Button>
+                    </Link>
+                </form>
+            </div>
+        );
+    }
 };

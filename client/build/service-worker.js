068fd7790c7a0a6304c612d2b4d5f9db17095a87
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-08-06 15:30:13 -0400

    updated

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae32902e0c41d1e08faf45735607fa32.js"
+  "/precache-manifest.9160bb48b0e4fe4e1cb37f715f6aeb76.js"
 );
 
 self.addEventListener('message', (event) => {

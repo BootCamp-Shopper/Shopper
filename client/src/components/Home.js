960ae5d0a4bca804cf6fec45a46e65bdb73a14e5
Kat Kim <56666457+katkim0307@users.noreply.github.com>
2020-08-07 02:03:45 -0400

    Small fixes for warnings in Home.js and Items.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect} from "react";
 import { Link } from 'react-router-dom';
 
 export default function Home(props) {
@@ -8,15 +8,15 @@ export default function Home(props) {
   return (
     <div className="Home-content">
       <div className="Home-header">
-        <text className="text-logo">The Qcc Shopper</text>
+        <div className="text-logo">The QCC Shopper</div>
         <img src="marvel.jpg" alt="" width="570" height="250"/>
       </div>
 
       <div className="Home-footer"> 
          <div>
             <h1> Marvel Fan? </h1>
-            <text> and not a part of our marvelous community? </text>
-            <text> Well, hurry up and join in the fun by signing up for an account and grabbing your own unique superpower! </text>
+            <div> and not a part of our marvelous community? </div>
+            <div> Well, hurry up and join in the fun by signing up for an account and grabbing your own unique superpower! </div>
             <div className="sign-up-container">
                {isSignedIn ? <Link to="/login" onClick={() => handleClick('/login')}> <button className="sign-up-btn"> Login </button> </Link> : <Link to="/signup" onClick={() => handleClick('/signup')}> <button className="sign-up-btn"> Sign up </button> </Link>}
             </div>
@@ -27,9 +27,9 @@ export default function Home(props) {
          </div>
          <div>
             <h1> About: </h1>
-            <text> In this site, you'll find some of the most awesome, powerful superpowers known to man, and it all can be within your grasp! </text>
-            <text> But of course, there's a catch. Since you can't find these powers anywhere else in the world, the prices can be quite exorbitant. </text>
-            <text> Nonetheless, you won't regret it..Happy shopping! </text>
+            <div> In this site, you'll find some of the most awesome, powerful superpowers known to man, and it all can be within your grasp! </div>
+            <div> But of course, there's a catch. Since you can't find these powers anywhere else in the world, the prices can be quite exorbitant. </div>
+            <div> Nonetheless, you won't regret it..Happy shopping! </div>
          </div>
       </div>
 
--- client/src/components/Items.js
+++ client/src/components/Items.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
-import Axios from 'axios'; 
+import Axios from 'axios';
 import { Card, CardColumns, Button, Form } from 'react-bootstrap';
 
 const style = {
@@ -27,7 +27,7 @@ export default class Items extends Component {
         try {
             const response = await fetch("/api/items", { method: "GET" });
             const itemsData = await response.json();
- 
+
             this.setState({
                 items: itemsData,
                 loading: false,
@@ -40,15 +40,15 @@ export default class Items extends Component {
     //Delete button functionality for items
     handleClick = async (id, evt) => {
         try {
-            const {status} = await Axios.delete(`/api/items/${id}`) //route listing
+            const { status } = await Axios.delete(`/api/items/${id}`) //route listing
             if (status === 200) {
                 //creates the new item list without the deleted item; filter is called to return list of items barring whichever item was flagged as deleted; react re-renders page immediately when state changes
                 const newItemList = this.state.items.filter(item => item.id !== id)
                 this.setState({
                     items: newItemList
                 })
-            }        
-        } 
+            }
+        }
         catch (error) {
             console.error(error)
         }
@@ -58,41 +58,41 @@ export default class Items extends Component {
         evt.preventDefault();
 
         Axios.post('/api/items/', {
-           name: evt.target.name.value,
-           superhero: evt.target.superhero.value,
-           imageUrl: evt.target.imageUrl.value,
-           price: evt.target.price.value,
-           description: evt.target.description.value,
+            name: evt.target.name.value,
+            superhero: evt.target.superhero.value,
+            imageUrl: evt.target.imageUrl.value,
+            price: evt.target.price.value,
+            description: evt.target.description.value,
         })
-        .then(res => {
-            // insert the new item at the beginning of the items array
-            const { items } = this.state;
-            items.unshift(res.data);
-            
-            // reset the forms fields on submit
-            document.getElementById('new-item').reset();
+            .then(res => {
+                // insert the new item at the beginning of the items array
+                const { items } = this.state;
+                items.unshift(res.data);
 
-            // re-render state to display updated array
-            this.setState({
-               items: items,
-               loading: false,
+                // reset the forms fields on submit
+                document.getElementById('new-item').reset();
+
+                // re-render state to display updated array
+                this.setState({
+                    items: items,
+                    loading: false,
+                });
+            })
+            .catch(err => {
+                console.error(err.stack);
             });
-         })
-        .catch(err => {
-            console.error(err.stack);
-        });
     }
 
     addOrder = evt => {
         Axios.post('/api/orders/add', {
             superheroId: evt.target.getAttribute('itemID'),
         })
-        .then((res) => {
-           console.log(res.data);
-        })
-        .catch(err => {
-           console.error(err.stack);
-        });
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch(err => {
+                console.error(err.stack);
+            });
     }
 
     render() {
@@ -106,56 +106,58 @@ export default class Items extends Component {
 
         return (
             <div>
-                <div className="items-form">
-                   <Form id="new-item" onSubmit={evt => this.handleSubmit(evt)} style={style}>
-                      <Form.Group>
-                         <Form.Label>Name</Form.Label>
-                         <Form.Control name="name" type="text" placeholder="Enter superpower name."/>
-                      </Form.Group>
-                      <Form.Group>
-                         <Form.Label>Superhero</Form.Label>
-                         <Form.Control name="superhero" type="text" placeholder="Owner of superpower."/>
-                      </Form.Group>
-                      <Form.Group>
-                         <Form.Label>Image URL</Form.Label>
-                         <Form.Control name="imageUrl" type="url" placeholder="Please provide direct image link."/>
-                      </Form.Group>
-                      <Form.Group>
-                         <Form.Label>Price</Form.Label>
-                         <Form.Control name="price" type="number" placeholder="Cost of item"/>
-                      </Form.Group>
-                      <Form.Group>
-                         <Form.Label> Description </Form.Label>
-                         <Form.Control as='textarea' rows="2" name="description" type="text" placeholder="Include small description of power here."/>
-                      </Form.Group>
-                      
-                      <Button variant="primary" type="submit">
-                           Submit
-                      </Button>
-                      
-                   </Form>
-                
-                </div>
-               
-                <br />
-                <hr />
-                <br />
-            
-                <CardColumns style={{marginLeft: '5%', marginRight: '5%'}}>
-                    {items.map(item => {
-                        return (
+                <div>
+                    <CardColumns style={{ marginLeft: '5%', marginRight: '5%' }}>
+                        {items.map(item => {
+                            return (
                                 <Card key={item.id}>
                                     <Card.Img variant="top" src={item.imageUrl} alt='' />
                                     <Card.Body>
-                                    <Card.Title><Link to={`/superpowers/${item.id}`}> {item.superhero}'s {item.name} </Link></Card.Title>
-                                    <Card.Text>${item.price}.00</Card.Text>
-                                    <Button variant='primary' type='button' value="Add to cart" itemID={item.id} onClick={(evt) => this.addOrder(evt)} style={{marginRight: '3%'}}>Add To Cart</Button>
-                                    <Button variant='danger' type='button' onClick={() => {this.handleClick(item.id)}}>Delete Item</Button>
+                                        <Card.Title><Link to={`/superpowers/${item.id}`}> {item.superhero}'s {item.name} </Link></Card.Title>
+                                        <Card.Text>${item.price}.00</Card.Text>
+                                        <Button variant='primary' type='button' value="Add to cart" itemID={item.id} onClick={(evt) => this.addOrder(evt)} style={{ marginRight: '3%' }}>Add To Cart</Button>
+                                        <Button variant='danger' type='button' onClick={() => { this.handleClick(item.id) }}>Delete Item</Button>
                                     </Card.Body>
                                 </Card>
-                        )
-                    })}
-                </CardColumns>
+                            )
+                        })}
+                    </CardColumns>
+                </div>
+
+                <br />
+                <hr />
+                <br />
+
+                <div className="items-form">
+                    <Form id="new-item" onSubmit={evt => this.handleSubmit(evt)} style={style}>
+                        <Form.Group>
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control name="name" type="text" placeholder="Enter superpower name." />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Superhero</Form.Label>
+                            <Form.Control name="superhero" type="text" placeholder="Owner of superpower." />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Image URL</Form.Label>
+                            <Form.Control name="imageUrl" type="url" placeholder="Please provide direct image link." />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Price</Form.Label>
+                            <Form.Control name="price" type="number" placeholder="Cost of item" />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label> Description </Form.Label>
+                            <Form.Control as='textarea' rows="2" name="description" type="text" placeholder="Include small description of power here." />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                      </Button>
+
+                    </Form>
+
+                </div>
             </div>
         );
     };
